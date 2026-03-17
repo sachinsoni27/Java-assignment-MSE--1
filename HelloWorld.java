4b0ec7731e21dcbd7adfc6b5af13f890b2731dfc
sachinsoni27 <sachinsoni27@gmail.com>
2026-03-17 15:36:40 +0530

    Add HelloWorld.java sample

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Sample Java Program
+ * MSE - 1 Assignment
+ */
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello from Java Assignment MSE - 1!");
+        
+        // Add your code here
+        System.out.println("Welcome to your Java project");
+    }
+}
